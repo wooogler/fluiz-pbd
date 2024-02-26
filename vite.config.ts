8be806eab983a075d39caed0f679cc2c6a83a5e7
wooogler <leesang627@gmail.com>
2024-02-26 14:52:14 +0900

    feat: project init

--- vite.config.ts
+++ vite.config.ts
@@ -52,6 +52,12 @@ export default defineConfig({
           return `assets/[ext]/${assetFileName}.chunk.[ext]`;
         },
       },
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE' && warning.message.includes(`"use client"`)) {
+          return;
+        }
+        warn(warning);
+      },
     },
   },
   test: {
