5b7ce60bdc8a8d341e95079e5f66bc01c129d9ed
wooogler <leesang627@gmail.com>
2024-02-27 10:43:18 +0900

    feat: replace innerText to uniqueId

--- src/pages/content/injected/detectElement.ts
+++ src/pages/content/injected/detectElement.ts
@@ -3,9 +3,26 @@ import refreshOnUpdate from 'virtual:reload-on-update-in-view';
 
 refreshOnUpdate('pages/content/injected/detectElement');
 
+function getElementUniqueId(element: HTMLElement): string {
+  const representativeAttrs = ['id', 'class', 'name', 'role', 'type', 'aria-label', 'href'];
+  const uniqueAttrs: string[] = [];
+  if (element.textContent) {
+    uniqueAttrs.push(element.textContent.trim().slice(0, 20));
+  }
+  uniqueAttrs.push(element.tagName.toLowerCase());
+  for (const attr of representativeAttrs) {
+    const attrValue = element.getAttribute(attr);
+    if (attrValue) {
+      uniqueAttrs.push(`${attr}=${attrValue}`);
+    }
+  }
+  return uniqueAttrs.join(',');
+}
+
 document.addEventListener('mouseover', event => {
-  const hoverText = (event.target as HTMLElement).innerText;
-  if (hoverText) {
-    elementInfoStorage.update(hoverText);
+  const targetElement = event.target as HTMLElement;
+  const uniqueElementId = getElementUniqueId(targetElement);
+  if (uniqueElementId) {
+    elementInfoStorage.update(uniqueElementId);
   }
 });
